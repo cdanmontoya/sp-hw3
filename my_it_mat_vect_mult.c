1dc320ebf6f32ce3761866797b72a97b153d63a3
cdanmontoya <carlos.montoyah@udea.edu.co>
2020-11-16 06:01:17 -0500

    Adding gathering at print

--- my_it_mat_vect_mult.c
+++ my_it_mat_vect_mult.c
@@ -19,14 +19,23 @@ void mat_vect_mult(double* A, double* x, double* y, int n, int it);
 /* función para imprimir un vector llamado <name> de tamaño <m>*/
 void print_vector(char* name, double*  y, int m);
 
-void Mat_vect_mult(
+void Mat_vect_mult_par(
   double    local_A[]  /* in  */, 
   double    local_x[]  /* in  */, 
   double    local_y[]  /* out */,
   int       local_m    /* in  */, 
   int       n          /* in  */,
   int       local_n    /* in  */,
+  int iters,
   MPI_Comm  comm       /* in  */);
+
+void Print_vector_par(
+  char      title[]     /* in */, 
+  double    local_vec[] /* in */, 
+  int       n           /* in */,
+  int       local_n     /* in */,
+  int       my_rank     /* in */,
+  MPI_Comm  comm        /* in */);
 
 void scatter_matrix(
   double* A,
@@ -88,9 +97,11 @@ int main()
   scatter_matrix(A, local_A, n, local_n, my_rank, comm);
   scatter_vector(x, local_x, n, local_n, my_rank, comm);
 
-  Mat_vect_mult(local_A, local_x, local_y, local_n, n, local_n, comm);
+  Mat_vect_mult_par(local_A, local_x, local_y, local_n, n, local_n, iters, comm);
 
   print_vector("y", local_y, n);
+
+  Print_vector_par("Y par", local_y, n, local_n, my_rank, comm);
   MPI_Finalize();
   return 0;
 }
@@ -123,27 +134,36 @@ void print_vector(char* name, double*  y, int m) {
    printf("\n");
 }
 
-void Mat_vect_mult(
+void Mat_vect_mult_par(
       double    local_A[]  /* in  */, 
       double    local_x[]  /* in  */, 
       double    local_y[]  /* out */,
       int       local_m    /* in  */, 
       int       n          /* in  */,
       int       local_n    /* in  */,
+      int       iters,
       MPI_Comm  comm       /* in  */) {
    double* x;
-   int local_i, j;
+   int local_i, j, h;
    int local_ok = 1;
 
    x = malloc(n*sizeof(double));
    MPI_Allgather(local_x, local_n, MPI_DOUBLE,
          x, local_n, MPI_DOUBLE, comm);
 
-   for (local_i = 0; local_i < local_m; local_i++) {
+  
+  for (h = 0; h < iters; h++) {
+    for (local_i = 0; local_i < local_m; local_i++) {
       local_y[local_i] = 0.0;
       for (j = 0; j < n; j++)
          local_y[local_i] += local_A[local_i*n+j]*x[j];
-   }
+    }
+
+    for(local_i = 0; local_i < n; local_i++)
+      local_A[local_i] = local_y[local_i];
+  }
+
+   
    free(x);
 }
 
@@ -212,3 +232,30 @@ void scatter_vector(
   }
 
 }
+
+void Print_vector_par(
+      char      title[]     /* in */, 
+      double    local_vec[] /* in */, 
+      int       n           /* in */,
+      int       local_n     /* in */,
+      int       my_rank     /* in */,
+      MPI_Comm  comm        /* in */) {
+   double* vec = NULL;
+   int i, local_ok = 1;
+
+   if (my_rank == 0) {
+      vec = malloc(n*sizeof(double));
+      if (vec == NULL) local_ok = 0;
+      
+      MPI_Gather(local_vec, local_n, MPI_DOUBLE,
+            vec, local_n, MPI_DOUBLE, 0, comm);
+      printf("\nThe vector %s\n", title);
+      for (i = 0; i < n; i++)
+         printf("%f ", vec[i]);
+      printf("\n");
+      free(vec);
+   }  else {
+      MPI_Gather(local_vec, local_n, MPI_DOUBLE,
+            vec, local_n, MPI_DOUBLE, 0, comm);
+   }
+} 
